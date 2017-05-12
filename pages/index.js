603108fb2cbfe28bf64debe751177d8200b180e0
Nathan Rajlich <nathan@tootallnate.net>
2017-05-11 23:09:24 -0700

    first mobile fix
    Preload the stream URLs so that they are available upon the click event

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import Router from 'next/router'
 import debounce from 'debounce'
 import createDebug from 'debug'
@@ -17,7 +18,8 @@ export default class Search extends React.Component {
     this.audio = null
 
     this.state = {
-      nowPlaying: null
+      nowPlaying: null,
+      streams: null
     }
 
     this.onPlayClick = this.onPlayClick.bind(this)
@@ -29,37 +31,50 @@ export default class Search extends React.Component {
     if (!search) {
       search = '107.7'
     }
-    return await Search.search(search)
+    const { stations } = await iheart.search(search)
+    return { stations, search }
   }
 
-  static async search(value) {
-    const { bestMatch, stations } = await iheart.search(value)
-    return { bestMatch, stations, search: value }
+  async loadStream(stationId) {
+    const stream = (await iheart.streams(stationId))[0]
+
+    const url = await iheart.streamURL(stream)
+    debug('loaded stream URL: %o', url)
+
+    const { streams } = this.state
+    streams.set(stationId, url)
+    this.setState({ streams })
   }
 
   componentDidMount() {
     this.audio = new Audio()
+    this.setState({
+      streams: new Map
+    })
+    if (this.props.stations) {
+      for (const station of this.props.stations) {
+        console.log(station)
+        this.loadStream(station.id)
+      }
+    }
   }
 
-  async onSearchInput(err, value) {
+  onSearchInput(err, value) {
     Router.replace(`/?search=${encodeURIComponent(value)}`)
   }
 
-  async onPlayClick(e) {
+  onPlayClick(e) {
     const id = e.currentTarget.getAttribute('data-id') | 0
 
     this.stop()
+
+    const url = this.state.streams.get(id)
+    this.audio.src = url
+    this.audio.play()
+
     this.setState({
       nowPlaying: id
     })
-
-    const streams = await iheart.streams(id)
-    const stream = streams[0]
-
-    const url = await iheart.streamURL(stream)
-    debug('playing stream URL: %o', url)
-    this.audio.src = url
-    this.audio.play()
   }
 
   stop() {
@@ -74,6 +89,12 @@ export default class Search extends React.Component {
     const { nowPlaying } = this.state
     return (
       <div style={{ textAlign: 'center', padding: 40 }}>
+        <Head>
+          <title>iHeart Radio Search</title>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width; height=device-height" />
+        </Head>
+
         <h1>iHeart Radio Search</h1>
 
         <Input
