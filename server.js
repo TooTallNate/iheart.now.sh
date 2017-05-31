77942be8481ea0126fdd53afda089540eb8e1486
Nathan Rajlich <nathan@tootallnate.net>
2017-05-31 15:24:51 -0700

    add ICY proxy logic
    So if a URL responds with an ICY "http version" string,
    then we redirect through an ICY proxy which rewrites the
    http version as regular HTTP, for browser compatability.

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+const net = require('net')
+const tls = require('tls')
 const next = require('next')
 const micro = require('micro')
 const iheart = require('iheart')
+const once = require('event-to-promise')
+const { parse } = require('url')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+
+const ICY_PROXY = 'https://icy.n8.io/'
 
 app.prepare()
 .then(() => {
@@ -36,5 +42,42 @@ app.prepare()
 async function getStreamURL (stationId) {
   const stream = (await iheart.streams(stationId))[0]
   console.log(JSON.stringify(stream))
-  return await iheart.streamURL(stream)
+
+  let url = await iheart.streamURL(stream)
+  if (await isICY(url)) {
+    url = `${ICY_PROXY}${url}`
+  }
+  return url
+}
+
+async function isICY (url) {
+  const parsed = parse(url)
+
+  const secure = 'https:' === parsed.protocol
+  const host = parsed.host
+  const port = parsed.port || (secure ? 443 : 80)
+
+  const payload = [
+    `GET ${parsed.path} HTTP/1.1`,
+    `Host: ${parsed.host}`,
+    //'User-Agent: curl',
+    'Accept: *\/*',
+  ].join('\r\n') + '\r\n\r\n'
+
+  const socket = await connect(secure ? tls : net, { host, port })
+  socket.write(payload)
+
+  const firstChunk = await once(socket, 'data')
+  const firstSpace = firstChunk.indexOf(0x20)
+  const method = firstChunk.slice(0, firstSpace).toString('ascii').toUpperCase()
+
+  socket.destroy()
+
+  return 'ICY' === method
+}
+
+function connect (mod, opts) {
+  return new Promise((resolve, reject) => {
+    const socket = mod.connect(opts, (err) => err ? reject(err) : resolve(socket))
+  })
 }
